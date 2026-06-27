9a25c53dc840e747924d4bbfc32e596f54de6b49
7Swaize <rahejasachit@gmail.com>
2026-06-27 06:19:59 +0000

    Add recieving of paired sim cam frames on python side (#16)

--- go2/modules/video/sources/virtual_source.py
+++ go2/modules/video/sources/virtual_source.py
@@ -4,18 +4,18 @@ import numpy as np
 import iceoryx2 as iox2
 from typing import Optional
 from typing_extensions import override
-from iceoryx_interfaces.camera_data import DepthFrameData_, RGBFrameData_
+from iceoryx_interfaces.camera_data import FrameData_
 from iceoryx_interfaces.qos import CameraQoS
 
 from .camera_source import CameraSource
 from ..frame_result import FrameResult
 
+
 class VirtualCameraSource(CameraSource):
     def __init__(self) -> None:
-        self._thread = None
+        self._thread: Optional[threading.Thread] = None
         self._stop_event = threading.Event()
-        self._latest_rgb: Optional[np.ndarray] = None
-        self._latest_depth: Optional[np.ndarray] = None
+        self._latest_frames: Optional[tuple[np.ndarray, np.ndarray]] = None
         self._initialize_iox_services()
 
     @override
@@ -25,23 +25,14 @@ class VirtualCameraSource(CameraSource):
 
     def _initialize_iox_services(self) -> None:
         iox2.set_log_level_from_env_or(iox2.LogLevel.Error)
-        
+
         # Kill signal handling: https://github.com/eclipse-iceoryx/iceoryx2/issues/528
         self._node = iox2.NodeBuilder.new() \
                         .signal_handling_mode(iox2.SignalHandlingMode.Disabled) \
                         .create(iox2.ServiceType.Ipc)
-        
-        self._depth_service = self._node.service_builder(iox2.ServiceName.new(CameraQoS.TOPIC_SIM_CAMERA_DEPTH)) \
-                                .publish_subscribe(DepthFrameData_) \
-                                .max_publishers(CameraQoS.MAX_PUBLISHERS) \
-                                .max_subscribers(CameraQoS.MAX_SUBSCRIBERS) \
-                                .subscriber_max_buffer_size(CameraQoS.SUBSCRIBER_MAX_BUFFER_SIZE) \
-                                .subscriber_max_borrowed_samples(CameraQoS.SUBSCRIBER_MAX_BORROWED_SAMPLES) \
-                                .history_size(CameraQoS.HISTORY_SIZE) \
-                                .open_or_create()
-        
-        self._rgb_service = self._node.service_builder(iox2.ServiceName.new(CameraQoS.TOPIC_SIM_CAMERA_RGB)) \
-                                .publish_subscribe(RGBFrameData_) \
+
+        self._cam_service = self._node.service_builder(iox2.ServiceName.new(CameraQoS.TOPIC_SIM_CAMERA)) \
+                                .publish_subscribe(FrameData_) \
                                 .max_publishers(CameraQoS.MAX_PUBLISHERS) \
                                 .max_subscribers(CameraQoS.MAX_SUBSCRIBERS) \
                                 .subscriber_max_buffer_size(CameraQoS.SUBSCRIBER_MAX_BUFFER_SIZE) \
@@ -49,48 +40,38 @@ class VirtualCameraSource(CameraSource):
                                 .history_size(CameraQoS.HISTORY_SIZE) \
                                 .open_or_create()
 
-        self._depth_sub = self._depth_service.subscriber_builder().create()
-        self._rgb_sub = self._rgb_service.subscriber_builder().create()
-        self._cycle_time = iox2.Duration.from_millis(1)            
+
+        self._cam_sub = self._cam_service.subscriber_builder().create()
+        self._cycle_time = iox2.Duration.from_millis(1)
 
 
-    def _iox_thread(self):
+    def _iox_thread(self) -> None:
         while not self._stop_event.is_set():
             self._node.wait(self._cycle_time)
 
-            while True:
-                sample = self._depth_sub.receive()
-                if sample is None:
-                    break
+            sample = self._cam_sub.receive()
+            if sample is None:
+                continue
 
-                msg = sample.payload().contents
-                self._add_depth_to_buffer(
-                    np.array(msg.data, copy=True, dtype=np.uint16).reshape((msg.height, msg.width))
-                )
+            msg = sample.payload().contents
+            h, w = msg.height, msg.width
 
-            while True:
-                sample = self._rgb_sub.receive()
-                if sample is None:
-                    break
+            rgb = cv2.cvtColor(
+                np.array(msg.rgb_data, copy=True, dtype=np.uint8 ).reshape((h, w, 3))[::-1],
+                cv2.COLOR_RGB2BGR,
+            )
+            depth = np.array(msg.depth_data, copy=True, dtype=np.uint16).reshape((h, w))[::-1]
 
-                msg = sample.payload().contents
-                self._add_rgb_to_buffer(
-                    np.array(msg.data, copy=True, dtype=np.uint8).reshape((msg.height, msg.width, 3))
-                )
-
-    def _add_rgb_to_buffer(self, rgb_np: np.ndarray) -> None:
-        rgb_frame = cv2.cvtColor(rgb_np[::-1], cv2.COLOR_RGB2BGR)
-        self._latest_rgb = rgb_frame
-
-    def _add_depth_to_buffer(self, depth_np: np.ndarray) -> None:
-        self._latest_depth = depth_np[::-1]
+            self._latest_frames = (rgb, depth)  # atomic under GIL
 
     @override
     def _get_frames(self) -> FrameResult:
-        if self._latest_rgb is None or self._latest_depth is None:
+        pair = self._latest_frames
+        if pair is None:
             return FrameResult.pending()
 
-        return FrameResult.color_and_depth(color=self._latest_rgb.copy(), depth=self._latest_depth.copy())
+        self._latest_frames = None
+        return FrameResult.color_and_depth(color=pair[0], depth=pair[1])
 
     @override
     def _shutdown(self) -> None:
